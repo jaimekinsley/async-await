8bf2d3401fe0c99d0425c46a9fc4cec68eee2cf6
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-22 14:02:53 -0700

    writes copyFile function and passes the test

---
+++ __tests__/copyFile.test.js
@@ -0,0 +1,34 @@
+const { copy } = require('../lib/copyFile');
+
+const fsPromises = require('fs').promises;
+
+describe('tests copy function', () => {
+  beforeAll(() => {
+    return fsPromises.writeFile('./copy-file.txt', 'this is a file for testing the copy function');
+  });
+
+  afterAll(() => {
+    return Promise.all([
+      fsPromises.unlink('./copy-file.txt'),
+      fsPromises.unlink('./new-copy-file.txt')
+    ]);
+  });
+
+  // it('can copy a file contents into a new file', () => {
+  //   return copy('./copy-file.txt', './new-copy-file.txt')
+  //     .then(() => {
+  //       return fsPromises.readFile('./new-copy-file.txt', { encoding: 'utf8' });
+  //     })
+  //     .then(newFile => {
+  //       expect(newFile).toEqual('this is a file for testing the copy function');
+  //     });
+  // });
+
+  it('can copy a file contents into a new file', async() => {
+    await copy('./copy-file.txt', './new-copy-file.txt');
+    const newFile = await fsPromises.readFile('./new-copy-file.txt', { encoding: 'utf8' });
+    expect(newFile).toEqual('this is a file for testing the copy function');
+  });
+});
+
+
